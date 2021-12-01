096291492c9092c7004b9a625b325d24aa6f113c
swanith123 <73668844+swanith123@users.noreply.github.com>
2021-12-01 16:52:38 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+function getParagraph1(){
+getElementById("showParagraph1").innerHTML = inputs.join(". ");
+}
+
+var inputs = [];
+
+for(var i = 1; i <=6; i++){
+    inputs.push(document.getElementById)("para1_input_box_" + i).value;
+}
+
+inputs.join(". ");
+
+function getParagraph2(){
+    getElementById("showParagraph2").innerHTML = inputs.join(". ");
+    }
+
+for(var i = 1 ; i <=6 ; i++)    {
+    inputs.push(document.getElementById("para1_input_box_" + i).value);
+}
+
+for(var i = 1 ; i <=6 ; i++)    {
+    inputs.push(document.getElementById("para2_input_box_" + i).value);
+}
+
+for(var i = 1 ; i <=6 ; i++)    {
+    inputs.push(document.getElementById("para3_input_box_" + i).value);
+}
+
+for(var i = 1 ; i <=6 ; i++)    {
+    inputs.push(document.getElementById("para4_input_box_" + i).value);
+}
+
+for(var i = 1 ; i <=6 ; i++)    {
+    inputs.push(document.getElementById("para5_input_box_" + i).value);
+}
+
+for(var i = 1 ; i <=6 ; i++)    {
+    inputs.push(document.getElementById("para6_input_box_" + i).value);
+}
